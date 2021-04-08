9e9f3a244237862c5fb81f04f03c98e5bb8ef19d
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-07 23:50:55 -0400

    Consolidate grid types

--- src/main/java/org/jacobarchambault/travelexpenses/App.java
+++ src/main/java/org/jacobarchambault/travelexpenses/App.java
@@ -10,6 +10,7 @@ import org.jacobarchambault.travelexpenses.amounts.NumberInput;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.HBox;
@@ -51,40 +52,31 @@ public class App extends Application {
 										new Insets(10),
 										new TitledPane(
 												"One-time expenses: ",
-												new MainExpensesGrid(
-														new Label("Airfare: "),
-														airFare,
-														new Label("Car Rental: "),
-														carRental,
-														new Label("Registration: "),
-														registration)),
+												new TwoColumnGrid(
+														new Control[] { new Label("Airfare: "), airFare,
+																new Label("Car Rental: "), carRental,
+																new Label("Registration: "), registration })),
 										new TitledPane(
 												"Per diem expenses",
-												new PerDiemExpenseGrid(
-														new Label("Days on the trip: "),
-														tripDays,
-														new Label("Meals (up to $47 per day): "),
-														meals,
-														new Label("Total parking fees (up to $20 per day): "),
-														parking,
-														new Label("Total taxi fees (up to $40 per day): "),
-														taxi,
-														new Label("Nightly lodging (up to $195 per day): "),
-														lodging)),
+												new TwoColumnGrid(
+														new Control[] { new Label("Days on the trip: "), tripDays,
+																new Label("Meals (up to $47 per day): "), meals,
+																new Label("Total parking fees (up to $20 per day): "),
+																parking,
+																new Label("Total taxi fees (up to $40 per day): "),
+																taxi,
+																new Label("Nightly lodging (up to $195 per day): "),
+																lodging })),
 										new TitledPane(
 												"Gas mileage reimbursement",
 												new HBox(
 														new Label("Total miles driven: ($.40 reimbursed per mile): "),
 														milesDriven)),
-										new LabelGrid(
-												new Label("Total expenses: "),
-												new Label("Allowable expenses: "),
-												new Label("Excess expenses: "),
-												new Label("Saved expenses: "),
-												totalExpenses,
-												allowedLabel,
-												excessLabel,
-												savedLabel),
+										new TwoColumnGrid(
+												new Control[] { new Label("Total expenses: "), totalExpenses,
+														new Label("Allowable expenses: "), allowedLabel,
+														new Label("Excess expenses: "), excessLabel,
+														new Label("Saved expenses: "), savedLabel }),
 										new HBox(new EventButton("Calculate", e -> {
 											final var basicAmount = expenses.total();
 											final var mealsTotal = meals.total();
---
+++ src/main/java/org/jacobarchambault/travelexpenses/TwoColumnGrid.java
@@ -0,0 +1,20 @@
+package org.jacobarchambault.travelexpenses;
+
+import java.util.List;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Control;
+import javafx.scene.layout.GridPane;
+
+public class TwoColumnGrid extends GridPane {
+
+	TwoColumnGrid(Control[] controls) {
+		for (var i = 0; i < controls.length; i++) {
+			add(controls[i], i % 2, i / 2);
+		}
+		setVgap(10);
+		setCenterShape(true);
+		setAlignment(Pos.CENTER);
+
+	}
+}
